0bfde9581fa02465dc30dfc88a17f0bd82dfd450
Jose Ramon Huerga <jrhuerga@gmail.com>
2022-06-22 23:52:55 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,14 @@ proxy_wasm::main! {{
     proxy_wasm::set_log_level(LogLevel::Trace);
     proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> {
         Box::new(HttpConfigHeaderRoot {
-            config: PolicyConfig::default()
+            secret: String::new()
         })
     });
 }}
 
 struct HttpConfigHeader {
     //header_content: String,
-    config: PolicyConfig
+    secret: String
 }
 
 impl Context for HttpConfigHeader {}
@@ -23,28 +23,28 @@ impl HttpContext for HttpConfigHeader {
     fn on_http_request_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
         info!("on_http_request_headers");
         //info!("self header content {}",self.header_content);
-        info!("self header content {}",self.config.secret_value);
-        Action::Continue
+        info!("self header content {}",self.secret);
+        Action::Continue]
     }
 
     fn on_http_request_body(&mut self, _body_size: usize, _end_of_stream: bool) -> Action {
         info!("on_http_request_body");
         //info!("self header content {}",self.header_content);
-        info!("self header content {}",self.config.secret_value);
+        info!("self header content {}",self.secret);
         Action::Continue
     }
 
     fn on_http_response_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
         info!("on_http_response_headers");
         //info!("self header content {}",self.header_content);
-        info!("self header content {}",self.config.secret_value);
+        info!("self header content {}",self.secret);
         Action::Continue
     }
 
     fn on_http_response_body(&mut self, _body_size: usize, _end_of_stream: bool) -> Action {
         info!("on_http_response_body");
         //info!("self header content {}",self.header_content);
-        info!("self header content {}",self.config.secret_value);
+        info!("self header content {}",self.secret);
         Action::Continue
     }
 }
@@ -57,7 +57,7 @@ struct PolicyConfig {
 
 struct HttpConfigHeaderRoot {
 //    header_content: String,
-    config: PolicyConfig
+    secret: String
 }
 
 impl Context for HttpConfigHeaderRoot {}
@@ -65,7 +65,8 @@ impl Context for HttpConfigHeaderRoot {}
 impl RootContext for HttpConfigHeaderRoot {
     fn on_configure(&mut self, _: usize) -> bool {
         if let Some(config_bytes) = self.get_plugin_configuration() {
-            self.config = serde_json::from_slice(config_bytes.as_slice()).unwrap();
+            let config = serde_json::from_slice(config_bytes.as_slice()).unwrap()
+            self.secret = config.secret_value;
             //let strConfig = String::from_utf8(config_bytes).unwrap();
             //let c: Config = serde_json::from_str(&strConfig);
             //self.header_content = c.secret_value;
@@ -78,7 +79,7 @@ impl RootContext for HttpConfigHeaderRoot {
 
     fn create_http_context(&self, _: u32) -> Option<Box<dyn HttpContext>> {
         Some(Box::new(HttpConfigHeader {
-            config: self.config.clone(),
+            secret: self.secret.clone(),
         }))
     }
 
